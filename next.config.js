e144090678fa703e6e8a0fc1878a4783ace5e641
jjunghoo <wjdgn2468@naver.com>
2022-07-08 18:29:37 +0900

    feat: nextJS에서 redirects와 rewrites 사용법
    source: 이동하게 될 주소
    destination: redirect 될 주소
    permanent: true 또는 false 인지에 따라서 브라우저나 검색엔진이 이 정보를 기억하는지 여부가 결정
    
    source: 이동하게 될 주소
    destination: redirect 될 주소 (redirects와 다르게 redirect되는 주소가 보여지지 않는다)
    *** API KEY 정보가 노출되지 않게 할 수 있다. ***

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = "9824191ad62a00268c4e56656c792cd3";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
