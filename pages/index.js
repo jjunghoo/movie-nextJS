8c7aa806eee8795fc89f242e72124d3384a78ef4
jjunghoo <wjdgn2468@naver.com>
2022-07-09 15:47:40 +0900

    feat: nextJS에서 Server Side Rendering
    getServerSideProps
    - 함수안에서 실행되는 내용들은 모두 서버에서만 실행이 된다.
    - return 하는 값은 props로써 page에 전달하게 된다.
    
    즉 server side를 통해 props를 page로 보낼 수 있다. 👀

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState();
+export default function Home({ results }) {
+  // const [movies, setMovies] = useState();
 
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      setMovies(results);
-    })();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+
+  //     setMovies(results);
+  //   })();
+  // }, []);
 
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loding...</h4>}
-      {movies?.map((movie) => (
+      {/* {!movies && <h4>Loding...</h4>} */}
+      {results?.map((movie) => (
         <div className="movie" key={movie.id}>
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
@@ -44,4 +44,16 @@ export default function Home() {
       `}</style>
     </div>
   );
+}
+
+// Server Side Rendering
+export async function getServerSideProps() {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
 }
